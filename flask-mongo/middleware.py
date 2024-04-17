93f55cd1fae90386169fb94f346ee8fe57337d55
Akash Kumar <meakash7902@gmail.com>
2024-04-17 16:26:45 +0530

    write coverage data in dedupdata.yaml
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- flask-mongo/middleware.py
+++ flask-mongo/middleware.py
@@ -1,16 +1,21 @@
 import coverage
 
+from werkzeug.wrappers import Request
+from utils import write_dedup
+
 class CoverageMiddleware:
     def __init__(self, app):
         self.app = app
 
     def __call__(self, environ, start_response):
+        req = Request(environ)
+        id = req.headers.get('KEPLOY-TEST-ID')
+        if id == None:
+            return self.app(environ, start_response)
         cov = coverage.Coverage(cover_pylib=False)
         cov.start()
         response = self.app(environ, start_response)
         cov.stop()
         result = cov.get_data()
-        for file in result.measured_files():
-            print(file, result.lines(file))
-
+        write_dedup(result, id)
         return response
